0432bdb618462bebf0d7a29a4a57d3f9e8ea4f68
Dimitri Topaloglou <dtopaloglou@gmail.com>
2016-01-13 14:05:29 -0500

    Fixed spl_autoload_register for Linux based system

--- includes/dbc.php
+++ includes/dbc.php
@@ -1,9 +1,9 @@
 <?php
 error_reporting(E_ALL ^ E_NOTICE);
 function my_autoloader($class){
-    $f = $_SERVER['DOCUMENT_ROOT']."/includes/". $class . '.php';
-    if(is_file($f))
-        include_once ($f);
+    $class = (str_replace('\\', DIRECTORY_SEPARATOR, $class));
+
+    require_once $class . '.php';
 }
 // spl_autoload_register will now include every class from within the "includes" folder to be used accross the application. Without this, classes must be included manually.
 spl_autoload_register('my_autoloader');
@@ -25,3 +25,5 @@ Registry::setConfig(new MySqlConfig(DatabaseManager::dbUser, DatabaseManager::db
  *
  */
 ?>
+
+
