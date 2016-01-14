ad87d63a0916dfbdaca81c27d7c3ab552d368287
Dimitri Topaloglou <dtopaloglou@gmail.com>
2016-01-13 19:17:05 -0500

    Explicitly  created MySqlConfig for clarification purposes

--- includes/dbc.php
+++ includes/dbc.php
@@ -8,8 +8,9 @@ function my_autoloader($class){
 // spl_autoload_register will now include every class from within the "includes" folder to be used accross the application. Without this, classes must be included manually.
 spl_autoload_register('my_autoloader');
 
-// Set database configuration using MySql confi driver
-Registry::setConfig(new MySqlConfig(DatabaseManager::dbUser, DatabaseManager::dbPass, DatabaseManager::dbName, DatabaseManager::host,  DatabaseManager::dbPort ));
+// Set database configuration using MySql config driver
+$MySqlConfig = new MySqlConfig(DatabaseManager::dbUser, DatabaseManager::dbPass, DatabaseManager::dbName, DatabaseManager::host,  DatabaseManager::dbPort );
+Registry::setConfig($MySqlConfig);
 
 
 
