e2a0bd6d10b73ad08b18fe8fe14a1b4adbf16e53
Dimitri Topaloglou <dtopaloglou@gmail.com>
2016-03-02 14:15:14 -0500

    #79 Hid tabs from un-authenticated users/

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -31,8 +31,8 @@
 				array('label'=>'Home', 'url'=>array('/site/index')),
 				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
 				array('label'=>'Contact', 'url'=>array('/site/contact')),
-				array('label'=>'Courses', 'url'=>array('/Courselist/index')),
-				array('label'=>'Users', 'url'=>array('/user')),
+				array('label'=>'Courses', 'url'=>array('/Courselist/index'), 'visible'=>!Yii::app()->user->isGuest),
+				array('label'=>'Users', 'url'=>array('/user'), 'visible'=>!Yii::app()->user->isGuest),
 				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
 				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
 			),
