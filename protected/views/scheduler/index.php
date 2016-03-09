7272e847f30fbd450fa4bd66afcf748a79f0b4a1
Dimitri Topaloglou <dtopaloglou@gmail.com>
2016-03-09 16:43:26 -0500

    Added preferences form.

--- protected/views/scheduler/index.php
+++ protected/views/scheduler/index.php
@@ -7,21 +7,29 @@
 $this->breadcrumbs=array(
 	'Scheduler',
 );
+
+
+$this->menu=array(
+    array('label'=>'View Saved Schedules', 'url'=>array('create')),
+);
 ?>
 
+<script>
+    $(function(){
+        console.log($('#yt0').serializeArray());
+    })
+</script>
 <div id="user-preferences">
     <h3>Select Preferences</h3>
     <?php
     // pass the model to the _preference.php file so that the data from the model can be accessed
     $this->renderPartial('_preference', array(
         'model' => $model,
+
     ));
     ?>
 </div>
 
 
-<div id="generate-button">
-    <input type="button" value="View Schedule">
-</div>
 
 
