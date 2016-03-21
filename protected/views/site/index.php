c4dd2e781f91ae62eab06a3ecbc3460203fb7ed8
Erin Benderoff <erinb_17@hotmail.com>
2016-03-21 17:15:53 -0400

    Made it so that it doesn't tell you to log in when already logged in.

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -4,15 +4,28 @@
 $this->pageTitle=Yii::app()->name;
 ?>
 
-<h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?>!</i></h1>
-<div id="login">
-    <p>Please <a href="index.php?r=site/login">click here</a> to log in.</p>
-    <?php if(isset($_GET["logout"]) && $_GET["logout"]==1)
-    {?>
+<h1>
+    Welcome to
+    <?php
+    echo CHtml::encode(Yii::app()->name);
+    if(Yii::app()->user->isGuest == false)
+        echo(", ".Yii::app()->user->name);
+    ?>!
+</h1>
+<?php if(Yii::app()->user->isGuest) {?>
+    <div class="login">Please
+        <?php echo(CHtml::link('click here',array('site/login'))); ?>
+        to log in.
+    </div>
+<?php }
+else
+    echo("Click the 'Schedule Planner' tab to get started.");
+?>
+<?php if(isset($_GET["logout"]) && $_GET["logout"]==1)
+{?>
     <div class="logoutSuccess">You have successfully logged out!</div>
-    <?php } ?>
+<?php } ?>
 
-</div>
 <div id="logo"><?php echo CHtml::image(Yii::app()->request->baseUrl.'/images/logo.jpg', "logo"); ?></div>
 
 <!--
